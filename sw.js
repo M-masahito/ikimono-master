4e08d44493cde0d09fc04fb1cc8962d943a9919d
M-masahito <masa_emi_102534@yahoo.co.jp>
2026-07-22 07:48:12 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='ikimono-v1',A=['./','./index.html','./styles.css','./data.js','./app.js'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
