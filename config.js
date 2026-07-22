34de4f2396584774843f28ed351e068820666ca8
M-masahito <masa_emi_102534@yahoo.co.jp>
2026-07-22 13:28:39 +0900

    Update AI_WORKER_URL in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Cloudflare Worker のURLを入れてください。
 // 例: https://ikimono-ai.xxxxx.workers.dev
 window.IKIMONO_CONFIG = {
-  AI_WORKER_URL: ""
+  AI_WORKER_URL: "https://ikimono-ai.masahito-mizote.workers.dev",
 };
